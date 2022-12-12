b6bd7d3c3ee7c92c5330bd6edbcc411db72f42a3
Sezer Ökmen <szrokmn34@gmail.com>
2022-12-12 12:48:45 +0300

    add axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
+import axios from "axios";
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
 
 const Home = () => {
+
+  const url = "https://tutorials-api-cw.herokuapp.com/api/tutorials";
+
+  const getTutorials = async() => {
+    const data = await axios(url);
+    console.log(data);
+  }
+
   return (
     <>
       <AddTutorial />
