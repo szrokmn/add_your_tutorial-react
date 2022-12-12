43a86798c883fe9534e94d7055ab81f31aebaefe
Sezer Ökmen <szrokmn34@gmail.com>
2022-12-12 12:58:36 +0300

    add useState, useEffect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,24 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
 
 const Home = () => {
 
+  const [tutorials, setTutorials] = useState([]);
+
   const url = "https://tutorials-api-cw.herokuapp.com/api/tutorials";
 
   const getTutorials = async() => {
-    const data = await axios(url);
+    const { data } = await axios(url);
     console.log(data);
+    setTutorials(data);
   }
 
+ useEffect(() => {
+  getTutorials();
+ }, []);
+ 
   return (
     <>
       <AddTutorial />
